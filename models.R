d2338f48a70d3069b259c5042438dd1dbd481dde
AlexDuo <31524434+AlexDuo@users.noreply.github.com>
2017-12-07 14:37:15 -0600

    10 models

--- models.R
+++ models.R
@@ -11,9 +11,6 @@ nba <- read.csv("d:/NBA_stats.csv")
 head(nba,2)
 
 
-
-
-
 #fit each regressor individually
 mod1 <- lm(salary ~ fgm, data = nba)
 mod2 <- lm(salary ~ fga, data = nba)
@@ -63,6 +60,8 @@ ggplot(data = nba)+
   geom_point(mapping = aes(x = salary, y = predicted3))+
   geom_path(x = seq(from = 0, to = 40000000, length = nrow(nba)), y = seq(from = 0, to = 40000000, length = nrow(nba)))
 
+summary(modnew3)
+
 # 3 points made+attemp+pps
 
 modnew4 <- lm(salary ~ three_pm+three_pa+pps, data = nba)
@@ -80,7 +79,8 @@ modnew5 <- lm(salary ~ two_pm+two_pa+pps, data = nba)
 nba <- mutate(nba, predicted5= predict(modnew5, nba))
 
 ggplot(data = nba)+
-  geom_point(mapping = aes(x = salary, y = predicted5))
+  geom_point(mapping = aes(x = salary, y = predicted5))+
+  geom_path(x = seq(from = 0, to = 40000000, length = nrow(nba)), y = seq(from = 0, to = 40000000, length = nrow(nba)))
 
 # ftm+fta+pps
 
@@ -93,3 +93,53 @@ ggplot(data = nba)+
   geom_path(x = seq(from = 0, to = 40000000, length = nrow(nba)), y = seq(from = 0, to = 40000000, length = nrow(nba)))
 
 
+# put two new variables into table fgr, three_pr, two_pr, and ftr  the rate = made/attemp
+
+nba <- mutate(nba, fgr= (fgm/fga))
+nba <- mutate(nba, three_pr= (three_pm/three_pa))
+nba <- mutate(nba, two_pr= (two_pm/two_pa))
+nba <- mutate(nba, ftr= (ftm/fta))
+#ignore missing data
+newdata <- na.omit(nba)
+
+modnew7 <- lm(salary ~ fgr+three_pr+two_pr+ftr, data = newdata)
+
+newdata <- mutate(newdata, predicted7= predict(modnew7, newdata))
+
+ggplot(data = newdata)+
+  geom_point(mapping = aes(x = salary, y = predicted7))+
+  geom_path(x = seq(from = 0, to = 40000000, length = nrow(newdata)), y = seq(from = 0, to = 40000000, length = nrow(newdata)))
+
+# modnew3 modify
+
+modnew3modify <- lm(salary ~ fgm+three_pm+ftm+two_pm+pps,data = nba)
+
+nba <- mutate(nba, predicted8 = predict(modnew3modify, nba))
+
+ggplot(data = nba)+
+  geom_point(mapping = aes(x = salary, y = predicted8))+
+  geom_path(x = seq(from = 0, to = 40000000, length = nrow(nba)), y = seq(from = 0, to = 40000000, length = nrow(nba)))
+
+summary(modnew3modify)
+
+# abandoned regressors all-in
+
+modnew9 <- lm(salary ~ fg_perc+three_p_perc+ft_perc+two_p_perc+afg_perc,data = nba)
+
+nba <- mutate(nba, predicted9 = predict(modnew9, nba))
+
+ggplot(data = nba)+
+  geom_point(mapping = aes(x = salary, y = predicted9))+
+  geom_path(x = seq(from = 0, to = 40000000, length = nrow(nba)), y = seq(from = 0, to = 40000000, length = nrow(nba)))
+
+# modnew3modify modify
+
+modnew3modify2 <- lm(salary ~ fgm+three_pm+ftm+two_pm+pps+fgr+three_pr+two_pr+ftr,data = newdata)
+
+newdata <- mutate(newdata, predicted10= predict(modnew3modify2, newdata))
+
+ggplot(data = newdata)+
+  geom_point(mapping = aes(x = salary, y = predicted10))+
+  geom_path(x = seq(from = 0, to = 40000000, length = nrow(newdata), color = "red"), y = seq(from = 0, to = 40000000, length = nrow(newdata)), color = "red")
+
+summary(modnew3modify2)
